d051ae70bc800680f2f92154c9efb6774776d8eb
zeshanziya <zeshan.ziya@axelerant.com>
2024-05-08 07:12:22 +0530

    get bin path

--- src/Composer/AxelerantDQCPlugin.php
+++ src/Composer/AxelerantDQCPlugin.php
@@ -85,7 +85,7 @@ class AxelerantDQCPlugin implements PluginInterface, EventSubscriberInterface
      */
     public function scriptEventAction(Event $event): void
     {
-        echo "AxelerantDQC: inside scriptEventAction - " . $event->getName() . '-' . $event->getComposer()->getInstallationManager()->getInstallPath($this->composer->getPackage())  . PHP_EOL;
+        echo "AxelerantDQC: inside scriptEventAction - " . $event->getName() . '-' . $this->composer->getConfig()->get('bin-dir')  . PHP_EOL;
         $this->copyFilesToProject();
     }
 
