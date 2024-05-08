216ad7c98508cca085db3c3856767334357cd3f8
zeshanziya <zeshan.ziya@axelerant.com>
2024-05-08 08:25:42 +0530

    guess project root dir

--- src/Composer/AxelerantDQCPlugin.php
+++ src/Composer/AxelerantDQCPlugin.php
@@ -65,21 +65,78 @@ class AxelerantDQCPlugin implements PluginInterface, EventSubscriberInterface
     }
 
     /**
-     * Copies files from plugin to the project root where it's installed.
+     * Copies files from plugin to the project where it's installed.
      */
     public function copyFilesToProjectRoot(Event $event): void
     {
-        // Determine the destination directory in the project
-        $destination = getcwd();
+        $destination = $this->locateProjectRoot();
+        if ($destination === FALSE) {
+            $this->io->writeError('<fg=red>Copying configuration file failed. Unable to determine project root dir. Please copy configuration files manually.</fg=red>');
+            return;
+        }
+        $pluginDirectory = realpath(__DIR__ . '/../../');
 
-        // Copy each file to the project
-        copy(__DIR__ . '/../../grumphp.yml.dist', $destination . '/grumphp.yml.dist');
-        copy(__DIR__ . '/../../phpcs.xml.dist', $destination . '/phpcs.xml.dist');
-        copy(__DIR__ . '/../../phpmd.xml.dist', $destination . '/phpmd.xml.dist');
-        copy(__DIR__ . '/../../phpstan.neon.dist', $destination . '/phpstan.neon.dist');
+        // Copy each file to the project root.
+        copy($pluginDirectory . '/grumphp.yml.dist', $destination . '/grumphp.yml.dist');
+        copy($pluginDirectory . '/phpcs.xml.dist', $destination . '/phpcs.xml.dist');
+        copy($pluginDirectory . '/phpmd.xml.dist', $destination . '/phpmd.xml.dist');
+        copy($pluginDirectory . '/phpstan.neon.dist', $destination . '/phpstan.neon.dist');
 
-        // Output message indicating the files are copied
-        $this->io->write('Config files copied successfully!');
+        // Output message indicating the files are copied with warning if exists.
+        if (file_exists($destination . '/grumphp.yml.dist')) {
+            $this->io->write('<fg=yellow>Configuration files are overwritten. Please watchout for any changes!</fg=yellow>');
+        }
+        else {
+            $this->io->write('<fg=green>Configuration files are copies successfully.</fg=green>');
+        }
+    }
+
+    /**
+     * Locate Project root path.
+     */
+    private function locateProjectRoot(): string|bool {
+        $paths = [
+            getcwd(),
+            dirname($this->composer->getConfig()->get('bin-dir'), 2),
+            dirname($this->composer->getConfig()->get('vendor-dir'), 1),
+        ];
+        return $this->guessPath($paths, 'composer.json');
+    }
+
+    /**
+     * Build path
+     *
+     * @param string $baseDir
+     * @param string $path
+     * @return string
+     */
+    private function buildPath(string $baseDir, string $path): string
+    {
+        return $baseDir.DIRECTORY_SEPARATOR.$path;
+    }
+
+    /**
+     * Guess path using filename.
+     *
+     * @param array $paths
+     * @param string $filename
+     * @return string|bool
+     */
+    private function guessPath(array $paths, string $filename): string|bool
+    {
+        $paths = array_filter($paths);
+
+        foreach ($paths as $path) {
+            if (!is_dir($path)) {
+                continue;
+            }
+
+            $filePath = $this->buildPath($path, $filename);
+            if (file_exists($filePath)) {
+                return $path;
+            }
+        }
+        return FALSE;
     }
 
 }
